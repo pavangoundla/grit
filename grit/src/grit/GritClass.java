6ddbc6a12bfa91400babd95bc95d10a1b69d7140
pavangoundla <pavangoundla0707@gmail.com>
2021-12-15 13:43:05 +0530

    modifications

--- grit/src/grit/GritClass.java
+++ grit/src/grit/GritClass.java
@@ -4,7 +4,7 @@ public class GritClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Grit Master1");
+		System.out.println("Grit Master");
 	}
 
 }
